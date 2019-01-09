7824bc3ba42fe5d49deb1f456d041889316a0a7c
Jiri Capek <georgejc@seznam.cz>
2019-01-09 11:29:30 +0100

    Adding functions to compare avg salary

--- economic.py
+++ economic.py
@@ -1,3 +1,4 @@
+from data import data_salary
 from data import data_cpi
 from data import data_inf
 
@@ -40,7 +41,31 @@ def yearly_inflation(start_year, end_year):
     """
     if start_year < end_year and start_year in data_inf.keys() and end_year in data_inf.keys():
         for i in range(start_year, end_year+1):
-            print("in",i,"cz inflation was",data_inf[i],"%")                 
+            print("in",i,"cz inflation was",data_inf[i],"%")  
+
+def avg_salary(start_year, end_year):
+    """
+    Returns avg salaray in Czech republic for input year
+    """
+    if start_year < end_year and start_year in data_salary.keys() and end_year in data_salary.keys():
+        for i in range(start_year, end_year+1):
+            print("in",i,"cz avg salary was",data_salary[i],"czk")             
+
+def compare_salary(start_year, end_year):  
+    """
+    Returns valuation of average czech salary
+    """
+    salary_start_year = data_salary[start_year]
+    salary_end_year = data_salary[end_year]
+    sal_diff = salary_end_year - salary_start_year
+    sal_percent = 100 * (sal_diff / salary_start_year)
+    return round(sal_percent)
+    
+
+def salary_deval(salary, minus):
+    # comparing minus_percent function and compare_salary function
+    # and finally returns net valuation of salary for input years
+    return salary - minus            
 
 
 # main function to call other functions and prompt user input
@@ -52,9 +77,15 @@ def main():
         if start_year > end_year:
             raise ValueError("Start year input must be lower than end year.")
          
-        print(calculate_inflation(start_year, end_year, amount))
-        print("Devaluation is: ", minus_percent(start_year, end_year), "per cent")
+        print("The final amount is: ",calculate_inflation(start_year, end_year, amount))
+        print("Devaluation is: ", minus_percent(start_year, end_year), "%")
         yearly_inflation(start_year, end_year)
+        #defining functions to compare for def salary_deval
+        salary = compare_salary(start_year, end_year)
+        minus = minus_percent(start_year, end_year)
+        avg_salary(start_year, end_year)
+        print("Valuation of salary is: ",compare_salary(start_year, end_year), "%")
+        print("Net valuation of salary is: ", salary_deval(salary, minus), "%")        
     
     except ValueError as ve:
         print(ve)
